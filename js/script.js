cc586e87c9b4ba4d692c7c00ec36d7621f9a4d94
Jeanmarie Vargas <jeanmarie.vargas@gmail.com>
2016-10-05 22:12:18 -0400

    added visualization

--- js/script.js
+++ js/script.js
@@ -14,14 +14,42 @@ $(window).ready(function(){
     /* I did not know what a "Uint8Array" was. It's actually an array of 8-bit integers
     Read more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array
     */
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
     
     function renderFrame() {
-        requestAnimationFrame(renderFrame);
+      //  requestAnimationFrame(renderFrame);
         analyser.getByteFrequencyData(frequencyData);
-        console.log(frequencyData); // now you can read the frequency data!
+     //   console.log(frequencyData); // now you can read the frequency data!
+        
+        console.log("length: " + frequencyData.length);
+        for(bin = 0; bin < frequencyData.length; bin++) {
+            var val = frequencyData[bin];
+            var red = val;
+            var green = 255-val;
+            var blue = val/2;
+            context.fillStyle = 'rgb('+red+', '+green+', '+blue+')';
+            context.fillRect(bin * 2,0,2,200);
+        }
+        requestAnimationFrame(renderFrame);
+    }
+    
+   
+    
+    var draw = function() {
+        for(bin = 0; bin < audioSource.streamData.length; bin++) {
+            var val = audioSource.streamData[bin];
+            var red = val;
+            var green = 255-val;
+            var blue = val/2;
+            context.fillStyle = 'rgb('+red+', '+green+', '+blue+')';
+            context.fillRect(bin * 2,0,2,200);
+        }
+        requestAnimationFrame(draw);
     }
     
     audio.play();
     renderFrame();
+ //   draw();
 });
 
