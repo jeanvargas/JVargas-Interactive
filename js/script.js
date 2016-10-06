cab1df1cd6909b6ed2c18e0f6c23e74397fdd9a4
Jeanmarie Vargas <jeanmarie.vargas@gmail.com>
2016-10-06 00:18:42 -0400

    finished

--- js/script.js
+++ js/script.js
@@ -21,35 +21,31 @@ $(window).ready(function(){
       //  requestAnimationFrame(renderFrame);
         analyser.getByteFrequencyData(frequencyData);
      //   console.log(frequencyData); // now you can read the frequency data!
-        
         console.log("length: " + frequencyData.length);
         for(bin = 0; bin < frequencyData.length; bin++) {
             var val = frequencyData[bin];
-            var red = val;
-            var green = 255-val;
-            var blue = val/2;
+            var red = 255-val;
+            var green = val/2;
+            var blue = val;
             context.fillStyle = 'rgb('+red+', '+green+', '+blue+')';
+            console.log(red+ " " + green + " " + blue);
             context.fillRect(bin * 2,0,2,200);
         }
         requestAnimationFrame(renderFrame);
     }
+      
+    window.addEventListener('resize', resizeCanvas, false);
     
-   
-    
-    var draw = function() {
-        for(bin = 0; bin < audioSource.streamData.length; bin++) {
-            var val = audioSource.streamData[bin];
-            var red = val;
-            var green = 255-val;
-            var blue = val/2;
-            context.fillStyle = 'rgb('+red+', '+green+', '+blue+')';
-            context.fillRect(bin * 2,0,2,200);
-        }
-        requestAnimationFrame(draw);
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight*.1;
+        audio.play();
+        renderFrame();
     }
     
-    audio.play();
-    renderFrame();
+    resizeCanvas();
+  //  audio.play();
+ //   renderFrame();
  //   draw();
 });
 
